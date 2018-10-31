167b6a6dda93c59240b2b8077ce96045c06b1dea
Jack YANG <jingkangy91@gmail.com>
2018-10-31 10:32:11 +0800

    set python shell interpreter
    Signed-off-by: Jack YANG <jingkangy91@gmail.com>

--- init.el
+++ init.el
@@ -321,6 +321,7 @@ explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (setq org-agenda-files (list "~/MEGA/Org/gtd.org"
                                "~/MEGA/Tongji/Org/dissertation.org"))
+  (setq python-shell-interpreter "~/.local/anaconda3/bin/python")
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
